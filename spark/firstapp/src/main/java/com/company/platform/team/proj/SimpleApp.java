bbf72fc2be8c6654bfc93e2e6db91e7e1e356f6f
heirish <lihaiyan@navercorp.com>
2018-06-15 11:43:57 +0800

    refactor firstapp

--- spark/firstapp/src/main/java/com/company/platform/team/proj/SimpleApp.java
+++ spark/firstapp/src/main/java/com/company/platform/team/proj/SimpleApp.java
@@ -1,21 +1,54 @@
 package com.company.platform.team.proj;
 
+import com.company.platform.team.proj.common.AppPrameters;
+import org.apache.commons.cli.*;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.Dataset;
 /**
  * Created by admin on 2018/6/13.
  */
 public class SimpleApp {
+    private static AppPrameters appParameters = new AppPrameters();
+
     public static void main(String[] args) {
-        String logFile = "/home/heirish/spark/README.md";
-        SparkSession session = SparkSession.builder().appName("Simple Application").getOrCreate();
-        Dataset<String> logData = session.read().textFile(logFile).cache();
+        try{
+            parseArgs(args);
+            System.out.println(appParameters.inputDataFile);
+            SparkSession session = SparkSession.builder().appName("Simple Application").getOrCreate();
+            Dataset<String> logData = session.read().textFile(appParameters.inputDataFile).cache();
 
-        long numAs = logData.filter(s->s.contains("a")).count();
-        long numBs = logData.filter(s->s.contains("b")).count();
+            long numAs = logData.filter(s->s.contains("a")).count();
+            long numBs = logData.filter(s->s.contains("b")).count();
 
-        System.out.println("Lines with a: " + numAs + ", lines with b: " + numBs  );
+            System.out.println("Lines with a: " + numAs + ", lines with b: " + numBs  );
+            session.stop();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-        session.stop();
+    private static void parseArgs(String[] args){
+        Options options  = new Options();
+        options.addOption("h", "help", false, "show help");
+        options.addOption("f", "file", true, "input file");
+
+        CommandLineParser parser = new BasicParser();
+        try{
+            CommandLine commands = parser.parse(options, args);
+            if (commands.hasOption("h")) {
+                showHelp(options);
+            }
+            if (commands.hasOption("f")) {
+                appParameters.inputDataFile= commands.getOptionValue("f");
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void showHelp(Options options) {
+        HelpFormatter formater = new HelpFormatter();
+        formater.printHelp("SimpleApp", options);
+        System.exit(0);
     }
 }
---
+++ spark/firstapp/src/main/java/com/company/platform/team/proj/common/AppPrameters.java
@@ -0,0 +1,8 @@
+package com.company.platform.team.proj.common;
+
+/**
+ * Created by admin on 2018/6/15.
+ */
+public class AppPrameters {
+    public String inputDataFile;
+}
