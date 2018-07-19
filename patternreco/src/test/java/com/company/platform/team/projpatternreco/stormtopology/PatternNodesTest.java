408191144d49723789b55f00d199ce9712dc91d6
heirish <lihaiyan@navercorp.com>
2018-07-19 17:23:00 +0800

    rename directory

--- patternreco/src/test/java/com/company/platform/team/projpatternreco/stormtopology/PatternNodesTest.java
+++
@@ -1,7 +0,0 @@
-package com.company.platform.team.projpatternreco.stormtopology;
-
-/**
- * Created by Administrator on 2018/7/15 0015.
- */
-public class PatternNodesTest {
-}
--- patternreco/startthriftclient.sh
+++
@@ -1,1 +0,0 @@
-/home/heirish/apps/maven/bin/mvn test -Dtest=com.company.platform.team.projpatternreco.sparkandhadoop.patternnodehelper.PatternNodeClientTest#startClientTest4
