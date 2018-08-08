259b1615640ebe4795469e7e7ea21f547cc8c905
heirish <lihaiyan@navercorp.com>
2018-08-08 18:00:41 +0800

    MysqlUtil

--- patternreco/scripts/init.sql
+++ patternreco/scripts/init.sql
@@ -1,4 +1,4 @@
-create table if not exists nelo2_pattern_nodes(
+create table if not exists pattern_nodes(
 project_id int(5) not null,
 pattern_level int(5) not null,
 pattern_key varchar(32) not null,
@@ -7,7 +7,7 @@ pattern varchar(1024) not null,
 CONSTRAINT PK_pattern_nodes PRIMARY KEY (project_id, pattern_level, pattern_key)
 );
 
-create table if not exists nelo2_projects(
+create table if not exists projects(
 id int(5) not null PRIMARY KEY,
 project_name varchar(32) not null
 );
